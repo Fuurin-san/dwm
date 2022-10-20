c1304ec8f7dd0dd5eb2a7dd426a78e2bc5914f7b
fuurin <zdf1205654975@gmail.com>
2022-10-21 00:08:50 +0800

    update

--- site-lisp/config/init-lsp-bridge.el
+++ site-lisp/config/init-lsp-bridge.el
@@ -10,5 +10,9 @@
 (require 'lsp-bridge)
 (global-lsp-bridge-mode)
 
+;;(setq acm-enable-quick-access t)
+;; 切换 lsp-bridge 英文补全
+(define-key lsp-bridge-mode-map (kbd "M-c e") 'lsp-bridge-toggle-sdcv-helper)
+
 (provide 'init-lsp-bridge)
 ;;; init-lsp-bridge.el ends here
