f4b0d7f8e81344dd21aa8950c57bea5e93aef230
fuurin <zdf1205654975@gmail.com>
2022-10-21 23:28:12 +0800

    增加 lsp-bridge 智能跳转

--- site-lisp/config/init-lsp-bridge.el
+++ site-lisp/config/init-lsp-bridge.el
@@ -2,17 +2,44 @@
 ;;; Commentary:
 ;;; Code:
 
-;;lsp-bridge配置
+;;; Require:
 (package-install 'posframe)
 (package-install 'markdown-mode)
+(package-install 'dumb-jump)
 (require 'yasnippet)
-(yas-global-mode 1)
 (require 'lsp-bridge)
+
+
+;;lsp-bridge配置
+(yas-global-mode 1)
 (global-lsp-bridge-mode)
 
 ;;(setq acm-enable-quick-access t)
 ;; 切换 lsp-bridge 英文补全
 (define-key lsp-bridge-mode-map (kbd "M-c e") 'lsp-bridge-toggle-sdcv-helper)
+
+;; 融合 `lsp-bridge' `find-function' 以及 `dumb-jump' 的智能跳转
+(defun lsp-bridge-jump ()
+  (interactive)
+  (cond
+   ((eq major-mode 'emacs-lisp-mode)
+    (let ((symb (function-called-at-point)))
+      (when symb
+        (find-function symb))))
+   (lsp-bridge-mode
+    (lsp-bridge-find-def))
+   (t
+    (require 'dumb-jump)
+    (dumb-jump-go))))
+
+(defun lsp-bridge-jump-back ()
+  (interactive)
+  (cond
+   (lsp-bridge-mode
+    (lsp-bridge-find-def-return))
+   (t
+    (require 'dumb-jump)
+    (dumb-jump-back))))
 
 (provide 'init-lsp-bridge)
 ;;; init-lsp-bridge.el ends here
