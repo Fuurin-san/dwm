2be12cc070f73b2bc5c0f2674f2a75a043c00ea6
Fuurin08 <zdf1205654975@gmail.com>
2023-01-08 14:44:44 +0800

    add submodule: disable-mouse

---
+++ site-lisp/config/init-disable-mouse.el
@@ -0,0 +1,12 @@
+;;; init-disable-mouse.el --- configuration disable mouse
+;;; Commentary:
+
+;;; require
+(require 'disable-mouse)
+
+;;; Code:
+(global-disable-mouse-mode)
+
+(provide 'init-disable-mouse)
+
+;;; init-disable-mouse.el ends here
