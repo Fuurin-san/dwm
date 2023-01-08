4870036138f0e8cf0a354f7ee5f117ed99ed5fd7
Fuurin08 <zdf1205654975@gmail.com>
2023-01-08 19:01:47 +0800

    update

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -24,10 +24,6 @@
 ;; "init-popweb"
 ;; "C-c")
 
-;;设置"C-n"与"C-p"为激活company补全时的上下切换鍵
-;;(define-key company-active-map (kbd "C-n") 'company-select-next)
-;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
-
 ;;设置C-h C-f为查询函数定义
 (global-set-key (kbd "C-h C-f") 'find-function)
 ;;设置C-h C-v为查询变量
@@ -37,14 +33,8 @@
 ;;set netease-cloud-music shortkey
 ;;(global-set-key (kbd "M-n") 'netease-cloud-music)
 
-;;set shortkey of dirvish
-;;(global-set-key (kbd "M-e") 'dirvish-dwim)
-;;(global-set-key (kbd "M-q") 'dirvish-quit)
 (global-set-key (kbd "C-c o") 'consult-file-externally)
 (global-set-key (kbd "C-c f") 'consult-locate)
-
-;;toggle-company-english-helper
-(global-set-key (kbd "M-n") 'toggle-company-english-helper)
 
 (provide 'init-key)
 ;;; init-key.el ends here
--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -35,16 +35,16 @@
 ;;(global-set-key (kbd "C-c r") 'org-capture)
 
 ;; 设置org标题1-8级的字体大小和颜色，颜色摘抄自monokai。;希望org-mode标题的字体大小和正文一致，设成1.0， 如果希望标题字体大一点可以设成1.2
-;;(custom-set-faces
-;;  '(org-level-1 ((t (:inherit outline-1 :height 1.15  :foreground "#51AFEF"))))
-;;  '(org-level-2 ((t (:inherit outline-2 :height 1.13  :foreground "#C678DD"))))
-;;  '(org-level-3 ((t (:inherit outline-3 :height 1.11  :foreground "#A9A1E1"))))
-;;  '(org-level-4 ((t (:inherit outline-4 :height 1.09  :foreground "#7CC3F3"))))
-;;  '(org-level-5 ((t (:inherit outline-5 :height 1.07  :foreground "#D499E5"))))
-;;  '(org-level-6 ((t (:inherit outline-6 :height 1.05  :foreground "#A8D7F7"))))
-;;  '(org-level-7 ((t (:inherit outline-7 :height 1.03  :foreground "#E2BBEE"))))
-;;  '(org-level-8 ((t (:inherit outline-8 :height 1.01  :foreground "#DCEFFB"))))
-;;  ) ;; end custom-set-faces
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.15  :foreground "#51AFEF"))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.13  :foreground "#C678DD"))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.11  :foreground "#A9A1E1"))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.09  :foreground "#7CC3F3"))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.07  :foreground "#D499E5"))))
+  '(org-level-6 ((t (:inherit outline-6 :height 1.05  :foreground "#A8D7F7"))))
+  '(org-level-7 ((t (:inherit outline-7 :height 1.03  :foreground "#E2BBEE"))))
+  '(org-level-8 ((t (:inherit outline-8 :height 1.01  :foreground "#DCEFFB"))))
+  ) ;; end custom-set-faces
 
 ;;org-mode theme
 ;;(package-install 'org-modern)
